5e30804959f57182a4480b0b792d78f065021f4d
Vasundhara Saxena <86097076+vasundharasaxena@users.noreply.github.com>
2024-10-22 15:16:44 +0530

    Create about.js

---
+++ about/about.js
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>G.H.SHARMA</title>
+        <link rel="icon" type="image/png" href="../media/FAVI.png">
+        <link rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.css">
+
+        <link rel="stylesheet" href="about.css">
+    </head>
+    <body>
+
+        <div id="cursor"></div>
+        <div id="landscapeMessage">Please rotate your phone and relaod</div>
+
+        <div id="main">
+            <div id="background">
+                <video id="videoBG" autoplay muted loop>
+                    <source src="../media/mbBG (1).mp4" type="video/mp4">
+                    <!-- Add additional video sources here for different formats -->
+                </video>
+                <div id="page1">
+                    <div id="page1-content">
+                        <nav id="navbar">
+                            <a href="../index.html"><button
+                                    id="logo">G.H.SHARMA</button></a>
+                            <div id="nav-allbtns">
+                                <div id="navigate">
+                                    <a href="./about.html"><button
+                                            class="nav-btns"
+                                            id="about_btn">ABOUT</button></a>
+                                    <a
+                                        href="https://drive.google.com/file/d/1pjjT_wQGfQbs4FMHce6Vwxp3kL-cqh_1/view"><button
+                                            class="nav-btns">RESUME</button></a>
+                                </div>
+                                <a href="mailto:govindharsh42@gmail.com"
+                                    id="a-free"><button id="freelance">AVAILABLE
+                                        FOR FREELANCE</button></a>
+                            </div>
+                            <div class="hamburger">
+                                <div class="bar"></div>
+                                <div class="bar"></div>
+                                <div class="bar"></div>
+                            </div>
+                        </nav>
+
+                        <div id="page1-top">
+                            <p> <span>The developer's role is like a considerate
+                                    host</span>
+                                <span>who anticipates their guests' needs
+                                    and convert</span>
+                                <span>them in something
+                                    interactive.</span></p>
+
+                            <div id="arrow"> SCROLL ↓</div>
+
+                            <div id="img-container">
+                                <img src="../media/dp2.jpg" alt>
+                            </div>
+                        </div>
+
+                        <div id="page1-bottom">
+                            <p> <span>↪ </span> ME in mind ?</p>
+                            <p>06 Jan / 2003</p>
+                            <p>INDIA</p>
+                            <p>
+                                <span>Strong focus on crafting user interfaces
+                                    that</span>
+                                <span> incorporate seamless motion and</span>
+                                <span>interactivity.</span>
+
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div id="page2">
+                    <div class="line" id="line1"></div>
+                    <div id="page2-content">
+
+                        <div id="pagetop">
+                            <p>01/</p>
+                            <p>/04</p>
+
+                        </div>
+
+                        <div id="page2-main">
+                            <p>about me</p>
+                            <h2>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <span>Hello, I'm Govind, a Final year
+                                    Computer</span>
+                                <span>Science Engineering student. I'm
+                                    passionate
+                                    about</span>
+                                <span>creating connected brands,
+                                    commerce
+                                    solutions,</span>
+                                <span> products,
+                                    and web experiences.</span>
+                                <br><br>
+                                <span>When I'm not developing, I enjoy
+                                    weightlifting,</span>
+                                <span>scoring goals on the pitch and
+                                    continuing my
+                                    quest</span>
+                                <span> to find the best samosas.</span>
+
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="page3">
+                    <div class="line" id="line2"></div>
+                    <div id="page3-content">
+                        <div id="pagetop">
+                            <p>02/</p>
+                            <p>/04</p>
+                        </div>
+
+                        <div id="page3-main">
+                            <p>my work <br> experience</p>
+
+                            <div id="page3-content">
+                                <div class="page3-container">
+                                    <h4>WEB DEVELOPER</h4>
+                                    <h3> ▫ SUVIDHA FOUNDATION <br> ▫
+                                        QNOX
+                                        ADVERTISINGS </h3>
+                                </div>
+                                <div class="page3-container">
+                                    <h4>OPEN SOURCE <br> DEVELOPER </h4>
+                                    <h3>▫ HACKOTBERFEST <br>▫ HACK2SKILL
+                                        SSOC
+                                        2.0
+                                    </h3>
+                                </div>
+                                <div class="page3-container">
+                                    <h4>CORE TEAM MEMBER </h4>
+                                    <h3>▫ GOOGLE-DSC SRCASW <br></h3>
+                                </div>
+                                <div class="page3-container">
+                                    <h4> MENTOR </h4>
+                                    <h3>▫ GSSoC - GirlScript <br> Summer of Code
+                                        ' 2024
+                                        <br></h3>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div id="page4">
+                    <div class="line" id="line3"></div>
+                    <div id="page4-content">
+
+                        <div id="pagetop">
+                            <p>03/</p>
+                            <p>/04</p>
+
+                        </div>
+
+                        <div id="page4-main">
+                            <p>skills</p>
+                            <h2>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                                <span> C++ </span> ▫ <span> PYTHON </span> ▫
+                                <span> JAVA </span> ▫
+                                <span>
+                                    HTML5 </span> ▫ <span> CSS </span> ▫ <span>
+                                    BOOTSTRAP
+                                </span> ▫ <span> JAVASCRIPT </span> ▫ <span>
+                                    GSAP
+                                </span> ▫ <span> REACT.JS </span> ▫ <span>
+                                    TAILWIND
+                                </span> ▫ <span> NEXT.JS </span> ▫ <span> REDUX
+                                </span> ▫ <span> JQUERY </span> ▫ <span> NODE.JS
+                                </span> ▫ <span> EXPRESS.JS </span> ▫ <span>
+                                    DJANGO
+                                </span> ▫ <span> PostgreSQL </span> ▫ <span>
+                                    CRUD
+                                    operations
+                                </span> ▫ <span> GIT </span> ▫ <span> FIREBASE
+                                </span> ▫ <span> FIGMA </span> ▫ <span> CANVA
+                                </span>
+                            </h2>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div id="page6">
+                    <div class="line" id="line5"></div>
+                    <div id="pagetop">
+                        <p>04/</p>
+                        <p>/04</p>
+                    </div>
+                    <div id="page6-content">
+                        <div id="connectbox">
+                            <h2>GOT A PROJECT IN MIND?</h2>
+                            <h1>LET'S CONNECT !</h1>
+
+                            <div class="eyes">
+                                <div class="eye">
+                                    <div class="ball"></div>
+                                </div>
+                                <div class="eye">
+                                    <div class="ball"></div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div id="socials">
+
+                            <div class="social-div">
+                                <a
+                                    href="https://www.linkedin.com/in/govind-harsh-sharma/">
+                                    <h1>LINKEDIN</h1>
+                                    <h1>LINKEDIN</h1>
+                                </a>
+                            </div>
+                            <div class="social-div">
+                                <a
+                                    href="https://www.instagram.com/ssh.harshu/">
+                                    <h1>INSTAGRAM</h1>
+                                    <h1>INSTAGRAM</h1>
+                                </a>
+                            </div>
+                            <div class="social-div">
+                                <a
+                                    href="https://twitter.com/GovindHarsh11">
+                                    <h1>TWITTER</h1>
+                                    <h1>TWITTER</h1>
+                                </a>
+                            </div>
+                            <div class="social-div">
+                                <a href="https://github.com/ghsharma">
+                                    <h1>GITHUB</h1>
+                                    <h1>GITHUB</h1>
+                                </a>
+                            </div>
+
+                        </div>
+                        <div id="message">
+                            <div>
+                                <h4>feel free to connect with me on
+                                    social .
+                                </h4>
+                            </div>
+                            <div>
+                                <h4> By GHS | ©2024 All rights
+                                    reserved.</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.js"></script>
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"
+            integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"></script>
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"
+            integrity="sha512-onMTRKJBKz8M1TnqqDuGBlowlH0ohFzMXYRNebz+yOcc5TQr/zAKsthzhuv0hiyUKEiQEQXEynnXCvNTOk50dg=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"></script>
+        <script src="about.js"></script>
+    </body>
+</html>
